1489502340923e5d0421661af9ef23070a9388d6
franco <x.francoruiz@gmail.com>
2023-08-21 01:52:51 -0300

    chore(color): add comments to test cases

--- packages/react/src/atoms/Color/Color.test.tsx
+++ packages/react/src/atoms/Color/Color.test.tsx
@@ -7,35 +7,47 @@ const props = {
 };
 
 test('first render', () => {
+    // render screen
     render(<Color hexCode={props.rgb} />);
 
+    // get component
     const color = screen.getByTestId('dsfColorContainer');
 
+    // assertions
     expect(color).toBeInTheDocument();
 });
 
 test('control exact rgb in backgroundColor property', () => {
+    // render screen
     render(<Color hexCode={props.rgb} />);
 
+    // get component
     const color = screen.getByTestId('dsfColorContainer');
 
+    // get styles of component
     const computedStyles = getComputedStyle(color);
     const backgroundColor = computedStyles.backgroundColor;
 
+    // assertions
     expect(backgroundColor).toBe('rgb(255, 0, 66)');
 });
 
 test('control how change background color using differents rgb values', () => {
+    // mock info
     const props = {
         rgb: 'rgb(0, 0, 0)',
     };
 
+    // render screen
     render(<Color hexCode={props.rgb} />);
 
+    // get component
     const color = screen.getByTestId('dsfColorContainer');
 
+    // get styles of component
     const computedStyles = getComputedStyle(color);
     const backgroundColor = computedStyles.backgroundColor;
 
+    // assertions
     expect(backgroundColor).toBe('rgb(0, 0, 0)');
 });
