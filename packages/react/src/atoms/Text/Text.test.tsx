ca1a12e057c08dea5ac87eafe06812d803dddfca
franco <x.francoruiz@gmail.com>
2023-08-21 22:03:18 -0300

    chore(text): add comments to test cases

--- packages/react/src/atoms/Text/Text.test.tsx
+++ packages/react/src/atoms/Text/Text.test.tsx
@@ -3,35 +3,47 @@ import { render, screen } from '@testing-library/react';
 import Text from './Text';
 
 test('render Text component', () => {
+    // render screen
     render(<Text>Hola Mundo!</Text>);
 
+    // get component
     const text = screen.getByTestId('dsfText');
 
+    // assertions
     expect(text).toBeInTheDocument();
 });
 
 test('comprobe text value', () => {
+    // render screen
     render(<Text>Hola Mundo!</Text>);
 
+    // get component
     const text = screen.getByTestId('dsfText');
 
+    // assertions
     expect(text).toHaveTextContent('Hola Mundo!');
 });
 
 test('comprobe the initial value of classname', () => {
+    // render screen
     render(<Text>Hola Mundo!</Text>);
 
+    // get component
     const text = screen.getByTestId('dsfText');
 
+    // assertions
     expect(text).toBeInTheDocument();
     expect(text).toHaveClass('dsf-text-base');
 });
 
 test('comprobe how change the classname', () => {
+    // render screen
     render(<Text size="sm">Hola Mundo!</Text>);
 
+    // get component
     const text = screen.getByTestId('dsfText');
 
+    // assertions
     expect(text).toBeInTheDocument();
     expect(text).toHaveClass('dsf-text-sm');
 });
